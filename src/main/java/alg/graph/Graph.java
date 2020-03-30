5daf765082325510d248df50ccd73c3c5a91be82
yclooper <yclooper@163.com>
2020-03-30 15:53:28 +0800

    init

--- src/main/java/alg/graph/Graph.java
+++ src/main/java/alg/graph/Graph.java
@@ -1,6 +1,7 @@
 package alg.graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 邻接表实现无向图
@@ -8,13 +9,13 @@ import java.util.LinkedList;
  */
 public class Graph {
 
-    private int count;
+    private int v;
     private LinkedList<Integer> [] link;
 
-    public Graph(int count) {
-        this.count=count;
-        link = new LinkedList[count];
-        for (int i = 0; i <count ; i++) {
+    public Graph(int v) {
+        this.v = v;
+        link = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
             link[i] = new LinkedList<Integer>();
         }
     }
@@ -23,4 +24,40 @@ public class Graph {
         link[s].add(t);
         link[t].add(s);
     }
+
+
+    public void bfs(int s, int t) {
+        if (s == t) return;
+        boolean[] visited = new boolean[v];
+        visited[s]=true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < link[w].size(); ++i) {
+                int q = link[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+    }
+
+    private void print(int[] prev, int s, int t) { // 递归打印s->t的路径
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t + " ");
+    }
+
 }
--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,10 +1,31 @@
+import alg.Practice;
+import alg.thk.NQueenProblem;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Demo {
 
 
     @Test
     public void func1() {
+        String ss = "134134124";
+        System.out.println(ss.contains("134"));
+    }
 
+    @Test
+    public void func2() {
+        Practice practice = new Practice();
+//        System.out.println(practice.hasGroupsSizeX(new int[]{1,1,1,1,1,0,0,0}));
+//        System.out.println(practice.gcd(5,3));
+
+        System.out.println(practice.lastRemaining(5, 3));
+    }
+
+    @Test
+    public void func3() {
+        NQueenProblem nQueenProblem = new NQueenProblem();
+        nQueenProblem.solveNQ();
     }
 }
