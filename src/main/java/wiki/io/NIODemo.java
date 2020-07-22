33fcfc35de10ef04a0d287de67e3eb490dcaf2e2
yclooper <yclooper@163.com>
2020-07-22 17:57:58 +0800

    init

--- src/main/java/wiki/io/NIODemo.java
+++ src/main/java/wiki/io/NIODemo.java
@@ -3,6 +3,7 @@ package wiki.io;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
 
 /**
  * Created by chen on 2020/7/21.
@@ -41,5 +42,17 @@ public class NIODemo {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    /**
+     * 使用buffer解析网络请求数据流
+     * @param channel
+     * @throws IOException
+     */
+    public void readHttpBuffer(SocketChannel channel) throws IOException {
+        ByteBuffer header = ByteBuffer.allocate(128);
+        ByteBuffer body = ByteBuffer.allocate(1024);
+        ByteBuffer[] bufferArray = new ByteBuffer[]{header, body};
+        channel.read(bufferArray);
     }
 }
